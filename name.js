e86ef7ee96a03694080392418ca7660d953f4220
Choharim <jhl8586@naver.com>
2020-11-04 13:27:17 +0900

    Add nameInput

--- name.js
+++ name.js
@@ -1,6 +1,40 @@
-const userName = document.querySelector(".name");
-const userNameInput = userName.querySelector("input");
-const userHello = document.querySelector(".helllo");
+const name = document.querySelector(".js-name");
+const input = name.querySelector("input");
+const hello = document.querySelector(".js-hello");
 
-function init(){}
+const LS_NAME = "userName";
+const SHOW = "showing";
+
+function saveName(text){
+  localStorage.setItem(LS_NAME,text);
+}
+
+function paintName(text){
+  name.classList.remove(SHOW);
+  hello.classList.add(SHOW);
+  hello.innerText = `Hello ${text} ^__^`;
+}
+
+function subHandle(event){
+  event.preventDefault();
+  const nameValue = input.value;
+  paintName(nameValue);
+  saveName(nameValue);
+}
+
+function askName(){
+  name.classList.add(SHOW);
+  name.addEventListener("submit",subHandle);
+
+}
+
+function init(){
+  const userName = localStorage.getItem(LS_NAME);
+  if( userName === null ){
+    askName();
+  }
+  else{
+    paintName(userName);
+  }
+}
 init();
